e2cb1d3a76ced4d17b7bb98513d389cf7d6ff1b5
Craig Walsh <craigwalshuk@gmail.com>
2021-12-08 23:14:31 +0000

    Adds a component to display a playing card

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { ICard, Orientation } from '@src/types';
+
+  export let card: ICard;
+  export let orientation: Orientation;
+</script>
+
+<div class="card-container">
+  {#if orientation === 'facedown'}
+    <img src={`images/cards/B1.svg`} alt="Face down playing card" />
+  {:else}
+    <img src={`images/cards/${card.suit}${card.rank}.svg`} alt="Face up playing card" />
+  {/if}
+</div>

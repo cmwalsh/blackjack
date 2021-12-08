43ff030ad4332f9b20245ec73b2766dfa9fd7542
Craig Walsh <craigwalshuk@gmail.com>
2021-12-08 22:56:54 +0000

    Changes tabs to spaces when indenting

--- svelte.config.js
+++ svelte.config.js
@@ -4,25 +4,25 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					'@src': path.resolve('./src'),
-					'@lib': path.resolve('./src/lib'),
-					'@components': path.resolve('./src/components')
-				}
-			}
-		}
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      resolve: {
+        alias: {
+          '@src': path.resolve('./src'),
+          '@lib': path.resolve('./src/lib'),
+          '@components': path.resolve('./src/components')
+        }
+      }
+    }
+  }
 };
 
 export default config;

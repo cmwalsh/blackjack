b9c7ea9bcdb9ae8225f4edc6ad840de7ed39c75c
Craig Walsh <craigwalshuk@gmail.com>
2021-12-09 18:05:39 +0000

    Adds the Player and CardTable components

---
+++ src/components/CardTable.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { IPlayer } from "@src/types";
+  import Player from "@components/Player.svelte";
+
+  export let player: IPlayer;
+</script>
+
+<div class="card-table">
+  <div class="dealer">
+    <Player player={player} />
+  </div>
+  <div class="player">
+    <Player player={player} />
+  </div>
+</div>
---
+++ src/components/Player.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { IPlayer } from "@src/types";
+  import Card from "@components/Card.svelte";
+
+  export let player: IPlayer;
+</script>
+
+<div class="player">
+  <div class="cards">
+    {#each player.cards as card}
+      <Card card={card} orientation={card.orientation} />
+    {/each}
+  </div>
+</div>

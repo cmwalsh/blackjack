e660078a436c5edd6dfa4b430624dd23274ad5a5
Craig Walsh <craigwalshuk@gmail.com>
2021-12-09 19:40:00 +0000

    Update the index route to show the CardTable component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { IPlayer } from "@src/types";
+  import { Player } from "@lib/player";
+  import CardTable from "@components/CardTable.svelte";
+
+  let dealer = new Player('Dealer');
+  let player = new Player('Player');
+  let players: IPlayer[] = [dealer, player];
+</script>
+
+<div class="game-container">
+  <CardTable players={players} />
+</div>
